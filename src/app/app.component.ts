ec61748f2450d6941f25c8737c5bed1a9c6c23e5
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-03-22 00:05:09 -0500

    Implemented lorem grabbing a random sentence.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'typings';
+  targetString = '';
+
+  ngOnInit(): void {
+    this.targetString = lorem.sentence();
+  }
 }
