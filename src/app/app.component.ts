05a88c0e5205d3cc2fceb5913592ea7e208c6184
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-03-27 23:05:29 -0500

    Implemented typing tracking using classes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,32 @@ export class AppComponent implements OnInit {
   title = 'typings';
   targetString = '';
   success = false;
+  isWrong: Array<Boolean>;
+  isCorrect: Array<Boolean>;
+  isDone: Array<Boolean>;
 
   ngOnInit(): void {
     this.targetString = lorem.sentence();
+    this.isWrong = [];
+    this.isCorrect = [];
+    this.isDone = [];
+    for (let i = 0; i < this.targetString.length; ++i) {
+      this.isWrong.push(false);
+      this.isDone.push(false);
+      this.isCorrect.push(false);
+    }
   }
 
   onKey(currentInput: String): void {
     this.success = currentInput === this.targetString;
+
+    // Go char by char and figure it out.
+    // Current one
+    let index = currentInput.length - 1;
+    if (index >= 0) {
+      this.isDone[index] = true;
+      this.isCorrect[index] = this.targetString[index] === currentInput[index];
+      this.isWrong[index] = this.targetString[index] !== currentInput[index];
+    }
   }
 }
